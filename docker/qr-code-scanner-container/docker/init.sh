e03926da2f72606a1a3e60fa81e1156040da3768
bartoszbrozek <bartekbrozek@gmail.com>
2020-09-19 18:52:32 +0200

    feat: install ssl

--- docker/qr-code-scanner-container/docker/init.sh
+++ docker/qr-code-scanner-container/docker/init.sh
@@ -9,3 +9,6 @@ cd /var/www/html; php artisan migrate --force;
 cd /var/www/html; php artisan db:seed;
 cd /var/www/html; npm install; npm run dev;
 
+sudo a2enmod ssl;
+sudo service apache2 restart;
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt;
