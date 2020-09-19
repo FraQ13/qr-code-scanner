fcf4869f715244f37b82c259e2fa746660d608bd
bartoszbrozek <bartekbrozek@gmail.com>
2020-09-19 11:07:38 +0200

    feat: layout adjustments

---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <nav class="navbar navbar-dark bg-primary navbar-expand-lg mb-4">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">QR Scanner Admin</a>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">History</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">Example Component</div>
+
+            <div class="card-body">I'm an example component.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
+</script>
